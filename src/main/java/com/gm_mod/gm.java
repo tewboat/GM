581e6f04929b88508f688c6dc27d5d0ed5491aed
Nelzg <yoda.2001.13@gmail.com>
2022-01-20 22:47:56 +0300

    Basic things

---
+++ src/main/java/com/gm_mod/gm.java
@@ -0,0 +1,33 @@
+package com.gm_mod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("gm_mod")
+public class gm
+{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public gm() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+}
